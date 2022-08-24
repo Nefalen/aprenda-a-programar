0c2bfb6fe1e8bef30cc53a52f197230c53833f3a
Fabrício A Silva <fabricioamaralsilva@yahoo.com.br>
2022-08-24 20:27:23 -0300

    Adicionado texto de intro

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,8 +4,9 @@ let profissao = "Engenheiro"
 let idade = 34
 let lugar_dos_sonhos = "Alemanha"
 
-    console.log(nome)
+    console.log('Olá,'${nome}! 'Somos aqui da agencia de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para' ${lugar_dos_sonhos'}. Confirma pra gente alguns dados? A sua idade é '${idade}', você é '${profissao}' e o seu email é '${email}'? Ficamos no aguardo. Muito obrigado e boa viagem )
     console.log(email)
     console.log(profissao)
     console.log(idade)
     console.log(lugar_dos_sonhos)
+    
