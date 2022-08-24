9e32e6b1b302abc76a6ddb22aa504eda70dabd21
Fabrício A Silva <fabricioamaralsilva@yahoo.com.br>
2022-08-24 20:12:49 -0300

    console.log...

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,10 @@ let email = "fabricioamaralsilva@yahoo.com.br"
 let profissao = "Engenheiro"
 let idade = 34
 let lugar_dos_sonhos = "Alemanha"
+
+    console.log(name)
+    console.log(email)
+    console.log(profissao)
+    console.log(idade)
+    console.log(lugar_dos_sonhos)
+        
