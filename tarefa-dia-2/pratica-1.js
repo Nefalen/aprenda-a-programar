8bdd5ea6cb2bfd8a043bce34e1b21b23b226b5ed
Fabrício A Silva <fabricioamaralsilva@yahoo.com.br>
2022-08-24 20:40:01 -0300

    Correção

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,8 +4,4 @@ let profissao = "Engenheiro"
 let idade = 34
 let lugar_dos_sonhos = "Alemanha"
 
-    console.log('Ola,'${nome}! 'Somos aqui da agencia de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aereas para'${lugar_dos_sonhos}. 'Confirma pra gente alguns dados? A sua idade e '${idade}', voce e '${profissao}' e o seu email e '${email}'? Ficamos no aguardo. Muito obrigado e boa viagem )
-    console.log(email)
-    console.log(profissao)
-    console.log(idade)
-    console.log(lugar_dos_sonhos)
+console.log(`Ola,${nome}! Somos aqui da agencia de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aereas para ${lugar_dos_sonhos}. Confirma pra gente alguns dados? A sua idade e ${idade}, voce e ${profissao} e o seu email e ${email}? Ficamos no aguardo. Muito obrigado e boa viagem`)
