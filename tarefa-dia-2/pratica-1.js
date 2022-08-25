7902d09bffdbe1ec8c9ab3930ced61717d5b52ab
Fabrício A Silva <fabricioamaralsilva@yahoo.com.br>
2022-08-24 21:04:10 -0300

    Teste

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -5,3 +5,4 @@ let idade = 34
 let lugar_dos_sonhos = "Alemanha"
 
 console.log(`Ola,${nome}! Somos aqui da agencia de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aereas para ${lugar_dos_sonhos}. Confirma pra gente alguns dados? A sua idade e ${idade}, voce e ${profissao} e o seu email e ${email}? Ficamos no aguardo. Muito obrigado e boa viagem`)
+
